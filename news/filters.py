e50d693a46bb41df14370f10fdd7f30d0e0645f7
Денис <straigaldvir@yandex.ru>
2024-03-19 09:38:23 +0400

    Final_Task_10

--- news/filters.py
+++ news/filters.py
@@ -1,25 +1,32 @@
 from django import forms
 from django_filters import FilterSet, ModelChoiceFilter, DateFilter, CharFilter
-from .models import Post, Author
+from django.utils.translation import gettext as _
+from .models import Post, Author, Category
 
 
 class PostFilter(FilterSet):
     title = CharFilter(
         field_name='title',
         lookup_expr='icontains',
-        label='Название заголовка'
+        label=_('Название заголовка')
     )
     author = ModelChoiceFilter(
         queryset=Author.objects.all(),
         lookup_expr='exact',
-        label='Автор',
-        empty_label='Все авторы',
+        label=_('Автор'),
+        empty_label=_('Все авторы'),
+    )
+    category = ModelChoiceFilter(
+        queryset=Category.objects.all(),
+        lookup_expr='exact',
+        label=_('Категория'),
+        empty_label=_('Все категории'),
     )
     datetime_post = DateFilter(
         field_name='datetime_post',
         widget=forms.DateInput(attrs={'type': 'date', 'class': 'form'}),
         lookup_expr='date__gte',
-        label='Дата'
+        label=_('Дата')
     )
 
     class Meta:
@@ -27,5 +34,6 @@ class PostFilter(FilterSet):
         fields = [
             'title',
             'author',
+            'category',
             'datetime_post'
         ]
