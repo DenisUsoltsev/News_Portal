4355723bfb5addb9d30977c4f0124515796012e2
Денис <straigaldvir@yandex.ru>
2024-03-19 16:21:39 +0400

    Final_Task_10

--- news/forms.py
+++ news/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import Group
 from allauth.account.forms import SignupForm
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from .models import Post
 
 
